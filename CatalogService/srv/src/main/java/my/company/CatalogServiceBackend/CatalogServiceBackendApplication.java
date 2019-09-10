c8a4eb2a0d10cf5dfc0bf5fa945e794af71d295d
KarenChen <cc2632@cornell.edu>
2019-09-10 12:24:31 +0800

    Add CatalogService project

---
+++ CatalogService/srv/src/main/java/my/company/CatalogServiceBackend/CatalogServiceBackendApplication.java
@@ -0,0 +1,13 @@
+package my.company.CatalogServiceBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogServiceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogServiceBackendApplication.class, args);
+	}
+
+}
